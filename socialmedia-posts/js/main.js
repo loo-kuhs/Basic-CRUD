4d07ab4a1e7a3abd8eda0d93138814b018c163b2
lucasbear <graffe.c@yahoo.com>
2022-08-19 17:53:20 -0500

    CR

---
+++ socialmedia-posts/js/main.js
@@ -0,0 +1,39 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+let data = {};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = `Post cannot be blank`;
+  } else {
+    msg.innerHTML = ``;
+    acceptData();
+  }
+};
+
+let acceptData = () => {
+  data["text"] = input.value;
+  createPost();
+};
+
+let createPost = () => {
+  posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+  `;
+  input.value = "";
+};
